8b32a6c6dab59821185d9bc474b46036b9d38ff0
Adiks <apacholak97@interia.pl>
2021-05-20 20:45:39 +0200

    Implementing a simple single sign-on application

---
+++ simple-single-sign-on-application/src/main/java/pl/adiks/simplesinglesignonapplication/SimpleSingleSignOnApplication.java
@@ -0,0 +1,13 @@
+package pl.adiks.simplesinglesignonapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSingleSignOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSingleSignOnApplication.class, args);
+    }
+
+}
---
+++ simple-single-sign-on-application/src/test/java/pl/adiks/simplesinglesignonapplication/SimpleSingleSignOnApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adiks.simplesinglesignonapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSingleSignOnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
